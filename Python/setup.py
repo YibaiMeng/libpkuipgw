5f128a3c51b326768d577e24c7131d5803dbcfc7
Meng Yibai <mengyibai@pku.edu.cn>
2019-07-10 19:41:37 +0800

    Fcuked up with pypi.. Incremented a version without doing anything.

--- Python/setup.py
+++ Python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="libpkuipgw",
-    version="0.2.1",
+    version="0.2.2",
     author="Meng Yibai",
     author_email="mengyibai@pku.edu.cn",
     license="MIT",
