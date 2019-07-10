1e562f25b65afc3a3af7a537a561fac8090a75fe
Meng Yibai <mengyibai@pku.edu.cn>
2019-07-10 19:30:03 +0800

    Conflict resolved

--- Python/setup.py
+++ Python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="libpkuipgw",
-    version="0.2.0",
+    version="0.2.1",
     author="Meng Yibai",
     author_email="mengyibai@pku.edu.cn",
     license="MIT",
