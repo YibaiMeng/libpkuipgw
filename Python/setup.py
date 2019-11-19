f5beebbb3c1bd09fd5fe2bd21a10dc956559e684
Meng Yibai <mengyibai@pku.edu.cn>
2019-11-19 23:48:57 +0800

    1. Reorganized the implementation of commandline flags parsing 2. Added support for disconnecting by IP

--- Python/setup.py
+++ Python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="libpkuipgw",
-    version="0.2.5",
+    version="0.2.6",
     author="Meng Yibai",
     author_email="mengyibai@pku.edu.cn",
     license="MIT",
